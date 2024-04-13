d870552790e8aef6b1480fd155e797d17d83ae4f
Alex Dasneves <adasneves127@gmail.com>
2024-04-13 17:15:39 -0400

    New work

--- .venv/bin/activate.fish
+++ .venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/adasneves/Desktop/src/EEPROM_Programmer/.venv"
+set -gx VIRTUAL_ENV "/home/adasneves/Desktop/src/eeprom/.venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
--- window.py
+++ window.py
@@ -12,8 +12,12 @@ class GUI_Interface:
              psg.Button("Get URL")],
             [psg.Multiline(size=(None, 33),font=('Courier New', 12), 
                            key="output")],
-            [psg.Button('Edit'), psg.Push(), psg.Button('Write'), psg.Push(), psg.Button('Exit')]]
-        self.window = psg.Window('HelloWorld', layout, size=(850,710))
+            [psg.Button('Edit'), psg.Push(), psg.Button('Write'), psg.Push(),
+             psg.Button('Open'), psg.FileBrowse(key='Browse', visible=False), psg.Push(),
+             psg.Button('Read'), psg.Push(), psg.FileSaveAs("Choose File",file_types=(("Binary Files", "*.bin"),)), psg.Button("Save"), psg.Push(),
+
+             psg.Button('Exit')]]
+        self.window = psg.Window('EPROM IDE', layout, size=(870,730))
         self.isValidData = False
         self.raw_data = None
 
@@ -23,12 +27,16 @@ class GUI_Interface:
                 bin_data = base64.b64decode(result)
                 self.raw_data = bin_data
                 f.write(bin_data)
+            self.update_from_raw()
+                
+    def update_from_raw(self):
+        with tempfile.TemporaryDirectory() as dir:
+            with open(dir + "/img.bin", 'wb') as f:
+                f.write(self.raw_data)
             proc = subprocess.Popen(["/usr/bin/hexdump", "-C", dir + "/img.bin"], stdout=subprocess.PIPE)
             
             output = proc.stdout.read().decode()
             self.window['output'].update(value=output)
-                
-    
     
     def open_hexedit(self):
         with tempfile.TemporaryDirectory() as dir:
@@ -41,19 +49,68 @@ class GUI_Interface:
             with open(dir + "/img.bin", 'rb') as f:
                 bin_data = f.read()
                 self.raw_data = bin_data
-            proc = subprocess.Popen(["/usr/bin/hexdump", "-C", dir + "/img.bin"], stdout=subprocess.PIPE)
-        
-            output = proc.stdout.read().decode()
-            self.window['output'].update(value=output)
-                
+
+            self.update_from_raw()
     
+    def writeEEPROM(self):
+        with tempfile.TemporaryDirectory() as dir:
+            with open(dir + "/img.bin", 'wb') as f:
+                f.write(self.raw_data)
+            proc = subprocess.Popen(["/usr/bin/xterm", "-e", "/usr/local/bin/minipro", "-p", "AT28C256", '-w', dir + "/img.bin"])
+            proc.communicate()
+    
+    def readFile(self,vals):
+#        layout =    [[psg.Input(key='_FILEBROWSE_', enable_events=True, visible=False)],
+#            [psg.FileBrowse(target='_FILEBROWSE_')],
+#            [psg.OK()],]
+
+#        window = psg.Window('My new window',layout,size=(100,100),modal=True)
+#        return
+        print(vals)
+        filename = ""
+        if vals.get('Browse', '') == '':
+            print("Please press Browse first")
+            filename = psg.popup_get_file("Open a File", no_window=True)
+        else:
+            filename=vals["Browse"]
+        print(vals["Browse"])
+        if(filename.endswith(".txt")):
+            with open(filename,"r") as f:
+                binbuffer=f.read().split("\n")[:-1]
+                print(binbuffer)
+                self.raw_data=b""
+                for b in binbuffer:
+                    bchar = chr(int(b,16))
+                    self.raw_data+=bchar.encode()
+        else:
+            with open(filename,"rb") as f:
+                self.raw_data=f.read()
+        self.update_from_raw()
+
+
+    def readEEPROM(self):
+        with tempfile.TemporaryDirectory() as dir:
+            proc = subprocess.Popen(["/usr/bin/xterm", "-e", "/usr/local/bin/minipro", "-p", "AT28C256", '-r', dir + "/img.bin"])
+            proc.communicate() 
+
+            with open(dir + "/img.bin", 'rb') as f:
+                self.raw_data = f.read()
+            self.update_from_raw()
+            self.isValidData = True
+    
+
+    def save_buffer(self, vals):
+        with open(vals['Choose File'], 'wb') as f:
+            f.write(self.raw_data)
+
+
     def __call__(self):
         while True:
             event, vals = self.window.read()
-            # print(event, vals)
+            print(event)
             if event in (None, 'Exit'):
                 break
-            if event == 'Get URL':
+            elif event == 'Get URL':
                 url: str = vals['url']
                 split_url = url.split('/')
                 if split_url[0] not in ('http:','https:'):
@@ -65,5 +122,13 @@ class GUI_Interface:
                     split_url.insert(3, "raw")
                 url = "/".join(split_url)
                 self.parse_result(requests.get(url).text)
-            if event == "Edit" and self.isValidData:
+            elif event == "Edit" and self.isValidData:
                 self.open_hexedit()
+            elif event == "Write" and self.isValidData:
+                self.writeEEPROM()
+            elif event == "Read":
+                self.readEEPROM()
+            elif event == "Save":
+                self.save_buffer(vals)
+            elif event == "Open":
+                self.readFile(vals)
